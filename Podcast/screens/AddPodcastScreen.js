f7e9ac9242ccbe2bab859784bc3034b6d102d86e
xmmt <mkmax472@gmail.com>
2020-09-17 12:24:19 +0300

    стили

--- Podcast/screens/AddPodcastScreen.js
+++ Podcast/screens/AddPodcastScreen.js
@@ -5,6 +5,9 @@ import RNPickerSelect from 'react-native-picker-select';
 import CheckBox from '@react-native-community/checkbox';
 import PickerIcon from '../icons/PickerIcon';
 import DocumentPicker from 'react-native-document-picker';
+import ImageIcon from '../components/ImageIcon';
+import UploadFileButton from '../components/UploadFileButton';
+import EditAudioButton from '../components/EditAudioButton';
 
 class AddPodcastScreen extends React.Component {
     state = {
@@ -38,24 +41,28 @@ class AddPodcastScreen extends React.Component {
     }
     render() {
         return (
-            <View>
+            <View style={styles.screen}>
                 <View
                     style={{
+                        marginHorizontal: 20,
+                        marginVertical: 20,
                         flexDirection: 'row'
                     }}>
                     <TouchableOpacity>
-                        <Text>--image picker--</Text>
+                        <ImageIcon />
                     </TouchableOpacity>
                     <View>
-                        <Text>Название</Text>
+                        <Text style={{marginLeft: 13, marginBottom: 10, ...styles.text}}>Название</Text>
                         <TextInput
+                            style={styles.textInput}
                             placeholder="Введите наззвание подкаста"
                             onChangeText={value => this.setState({podcastName: value})}
                         />
                     </View>
                 </View>
-                <Text>Описание подкаста</Text>
+                <Text style={{...styles.text, marginLeft: 20, marginBottom: 12}}>Описание подкаста</Text>
                 <TextInput
+                    style={{...styles.textInput, marginRight: 20}}
                     multiline={true}
                     onChangeText={value => this.setState({podcastDescription: value})}
                 />
@@ -66,25 +73,30 @@ class AddPodcastScreen extends React.Component {
                             alignItems: 'center',
                             justifyContent: 'center'
                         }}>
-                        <Text>Загрузите Ваш подкаст</Text>
-                        <Text>Выберите готовый аудиофайл из Вашего телефона и добавьте его</Text>
+                        <Text style={{...styles.header, marginTop: 30}}>Загрузите Ваш подкаст</Text>
+                        <Text style={{...styles.text2, marginHorizontal: 15, marginBottom: 20}}>Выберите готовый аудиофайл из Вашего телефона и добавьте его</Text>
                         <TouchableOpacity onPress={() => {this.pickAudio()}}>
-                            <Text>Загрузить файл</Text>
+                            <UploadFileButton />
                         </TouchableOpacity>
                     </View>
                 :
-                    <View>
-                        <Text>{this.state.pickedFile.name}</Text>
-                        <Text>Вы можете добавить таймкоды и скорректировать подкаст в режиме редактирования</Text>
+                    <View
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}>
+                        <Text style={{...styles.header, marginTop: 30}}>{this.state.pickedFile.name}</Text>
+                        <Text style={{...styles.text2, marginHorizontal: 15, marginBottom: 20}}>Вы можете добавить таймкоды и скорректировать подкаст в режиме редактирования</Text>
                         <TouchableOpacity onPress={() => {this.props.navigation.navigate('AudioEditScreen', this.state.pickedFile)}}>
-                            <Text>Редактировать аудиозапись</Text>
+                            <EditAudioButton />
                         </TouchableOpacity>
                     </View>
                 }
-                <Text>--line--</Text>
                 <View
                     style={{
-                        flexDirection: 'row'
+                        flexDirection: 'row',
+                        marginTop: 20
                     }}>
                     <CheckBox
                         value={this.state.inappropriateContent}
@@ -168,10 +180,65 @@ const pickerSelectStyles = StyleSheet.create({
       marginBottom: 30,
       paddingRight: 30, // to ensure the text is never behind the icon,
       color: 'black'
-    },
+    }
+});
+
+const styles = StyleSheet.create({
     iconContainer: {
       top: 8,
       right: 34,
+    },
+    screen: {
+        backgroundColor: "#FFFFFF"
+    },
+    text: {
+        //fontFamily: SF Pro Text;
+        'fontStyle': 'normal',
+        'fontWeight': 'normal',
+        'fontSize': 14,
+        'lineHeight': 18,
+        /* identical to box height, or 129% */
+        'letterSpacing': -0.154,
+        /* Light Text / Subhead */
+        'color': '#6D7885'
+    },
+    textInput: {
+        marginLeft: '5.5%',
+        "backgroundColor": "#F2F3F5",
+        "borderWidth": 0.5,
+        "borderColor": "rgba(0, 0, 0, 0.12)",
+        "borderStyle": "solid",
+        "borderTopLeftRadius": 10,
+        "borderTopRightRadius": 10,
+        "borderBottomRightRadius": 10,
+        "borderBottomLeftRadius": 10,
+        "height": 40,
+        "paddingLeft": 12,
+        marginBottom: 12
+    },
+    header: {
+        "fontStyle": "normal",
+        "fontWeight": "600",
+        "fontSize": 20,
+        "lineHeight": 24,
+        "textAlign": "center",
+        "letterSpacing": 0.38,
+        "color": "#000000",
+        marginBottom: 12
+    },
+    text2: {
+        //font-family: SF Pro Text;
+        'fontStyle': 'normal',
+        'fontWeight': 'normal',
+        'fontSize': 16,
+        'lineHeight': 20,
+        /* or 125% */
+        'display': 'flex',
+        'alignItems': 'center',
+        'textAlign': 'center',
+        'letterSpacing': -0.32,
+        /* Light Text / Secondary */
+        'color': '#818C99'
     }
 });
 
