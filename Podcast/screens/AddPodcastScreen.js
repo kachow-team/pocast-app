015bc15508a199b12a8f703b5954b8cc1b940d81
xmmt <mkmax472@gmail.com>
2020-09-17 10:10:35 +0300

    верстка, выборы пикера, документ пикер

--- Podcast/screens/AddPodcastScreen.js
+++ Podcast/screens/AddPodcastScreen.js
@@ -4,45 +4,95 @@ import { Button, View, SafeAreaView, ScrollView, Text, TextInput, TouchableOpaci
 import RNPickerSelect from 'react-native-picker-select';
 import CheckBox from '@react-native-community/checkbox';
 import PickerIcon from '../icons/PickerIcon';
+import DocumentPicker from 'react-native-document-picker';
 
 class AddPodcastScreen extends React.Component {
     state = {
+        pickedFile: null,
         inappropriateContent: false,
         excludeEpisode: false,
         podcastTrailer: false,
         availability: 'Всем пользователям'
     };
+    async pickAudio() {
+        try {
+            const res = await DocumentPicker.pick({
+              type: [DocumentPicker.types.audio],
+            });
+            console.log(
+              res.uri,
+              res.type, // mime type
+              res.name,
+              res.size
+            );
+          } catch (err) {
+            if (DocumentPicker.isCancel(err)) {
+              // User cancelled the picker, exit any dialogs or menus and move on
+            } else {
+              throw err;
+            }
+          }
+    }
     render() {
         return (
             <View>
-                <TouchableOpacity>
-                    <Text>--image picker--</Text>
-                </TouchableOpacity>
-                <Text>Название</Text>
-                <TextInput placeholder="Введите наззвание подкаста"></TextInput>
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}>
+                    <TouchableOpacity>
+                        <Text>--image picker--</Text>
+                    </TouchableOpacity>
+                    <View>
+                        <Text>Название</Text>
+                        <TextInput placeholder="Введите наззвание подкаста"></TextInput>
+                    </View>
+                </View>
                 <Text>Описание подкаста</Text>
                 <TextInput></TextInput>
-                <Text>Загрузите Ваш подкаст</Text>
-                <Text>Выберите готовый аудиофайл из Вашего телефона и добавьте его</Text>
-                <TouchableOpacity>
-                    <Text>-file input--</Text>
-                </TouchableOpacity>
+                <View
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}>
+                    <Text>Загрузите Ваш подкаст</Text>
+                    <Text>Выберите готовый аудиофайл из Вашего телефона и добавьте его</Text>
+                    <TouchableOpacity>
+                        <Text>-file input--</Text>
+                    </TouchableOpacity>
+                </View>
                 <Text>--line--</Text>
-                <CheckBox
-                    value={this.state.inappropriateContent}
-                    onValueChange={(newValue) => {this.setState({inappropriateContent: newValue})}}
-                />
-                <Text>Ненормативный контент</Text>
-                <CheckBox
-                    value={this.state.excludeEpisode}
-                    onValueChange={(newValue) => {this.setState({excludeEpisode: newValue})}}
-                />
-                <Text>Исключить эпизод из экспорта</Text>
-                <CheckBox
-                    value={this.state.podcastTrailer}
-                    onValueChange={(newValue) => {this.setState({podcastTrailer: newValue})}}
-                />
-                <Text>Трейлер подкаста</Text>
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}>
+                    <CheckBox
+                        value={this.state.inappropriateContent}
+                        onValueChange={(newValue) => {this.setState({inappropriateContent: newValue})}}
+                    />
+                    <Text>Ненормативный контент</Text>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}>
+                    <CheckBox
+                        value={this.state.excludeEpisode}
+                        onValueChange={(newValue) => {this.setState({excludeEpisode: newValue})}}
+                    />
+                    <Text>Исключить эпизод из экспорта</Text>
+                </View>
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}>
+                    <CheckBox
+                        value={this.state.podcastTrailer}
+                        onValueChange={(newValue) => {this.setState({podcastTrailer: newValue})}}
+                    />
+                    <Text>Трейлер подкаста</Text>
+                </View>
                 <RNPickerSelect
                     style={pickerSelectStyles}
                     useNativeAndroidPickerStyle={false}
